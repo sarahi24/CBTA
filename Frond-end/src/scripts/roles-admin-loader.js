51f8078b787b1f755091ff7ac4cec4600bcae37a
sarahi24 <66486090+sarahi24@users.noreply.github.com>
2026-02-12 12:39:32 -0600

    Delete roles-admin-loader.js

--- Frond-end/src/scripts/roles-admin-loader.js
+++
@@ -1,7 +0,0 @@
-import { AdminAPI } from '../utils/adminAPI.js';
-import AuthService from '../utils/authService.js';
-
-window.AdminAPI = AdminAPI;
-window.AuthService = AuthService;
-window.__adminAPILoaded = true;
-console.log('✅ AdminAPI cargada:', window.AdminAPI);
